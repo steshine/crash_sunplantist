65111686ecbd7e62345e83d1c23a65202d57b532
skychen <skychen@sohu-inc.com>
2017-01-17 23:21:12 +0800

    fck

--- sunplanist.py
+++ sunplanist.py
@@ -13,8 +13,10 @@ remoteArtileUrl = 'http://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=10050
 staticUrl = 'http://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=100505&profile_ftype=1&is_article=1&pagebar=0&pl_name=Pl_Official_MyProfileFeed__24&id=1005051927070524&script_uri=/u/1927070524&feed_type=0&page=2&pre_page=1&domain_op=100505&__rnd=1484618916861'
 path = 'D:/sunplanist/'
 domain = 'http://weibo.com/'
+areadyFile = 'db.bin'
 articleDomain = 'http://weibo.com/ttarticle/p/show?id='
 cookies = dict({'SUB': '_2A251eDr2DeRxGedH61IY9irPzj2IHXVWDCs-rDV_PUNbm9AKLW3lkW9jxBxPPHtTE2oL9Ro7x3uIQDsx4A..', 'SUBP': '0033WrSXqPxfM725Ws9jqgMF55529P9D9WWJOyNfRkxkNh0jS8bSgy.y5NHD95Qp1K571KqXe0-pWs4DqcjzMNxLMcyDdJ-t', 'ALF': '1516076582', 'SCF': 'AsyQefbOlYDylg4fCtw3H3QMPF2JV8CpO-vIxNV0cOhJwr1y9KpQLywbKICnoNJWuyQxr8Io4N75502Z5V8Z-Fo.', 'ALC': 'ac%3D0%26bt%3D1484540582%26cv%3D5.0%26et%3D1516076582%26scf%3D%26uid%3D1900964351%26vf%3D0%26vs%3D1%26vt%3D0%26es%3D76d319342811f47f070777c7a21408ad', 'sso_info': 'v02m6alo5qztZOdlrmzmoalrpmSmbWalpC9jJOksIyDpLaNg4y1jJDAwA==', 'tgc': 'TGT-MTkwMDk2NDM1MQ==-1484540582-xd-94656CFB8970062D24ADEEFB3C75B24F-1', 'LT': '1484540582'})
+contentCookie = {'Cookie':'SINAGLOBAL=180004545838.58127.1478226788825; TC-Ugrow-G0=02e35d9240e6933947925d24232af628; SSOLoginState=1484533995; TC-V5-G0=458f595f33516d1bf8aecf60d4acf0bf; wb_g_upvideo_1900964351=1; _s_tentry=login.sina.com.cn; Apache=4914359893033.586.1484533998141; ULV=1484533998155:8:2:1:4914359893033.586.1484533998141:1484014332879; TC-Page-G0=e2379342ceb6c9c8726a496a5565689e; UOR=www.csdn.net,widget.weibo.com,login.sina.com.cn; SCF=AunKxN5b-9Qj_8fHyis92yCtDUqeqE1j2tPOKNHhkkWvaaHsOYZP-QxFTjFndvhGUmQX-Xb7vsiPInnoKUwWkAA.; SUB=_2A251efdeDeRxGedH61IY9irPzj2IHXVWD2-WrDV8PUNbmtAKLU7QkW9OTxN_MGwdKyPi10rGDGtDF_OgzQ..; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWJOyNfRkxkNh0jS8bSgy.y5JpX5KMhUgL.Fo24eh54SoB0SK22dJLoI7y0UsH0IsLV9Btt; SUHB=010HR9Eb3Yh3k3; ALF=1516157581; wvr=6; WBStorage=ffbf906cea1ff551|undefined'}
 remoteHeaders = {'Host': 'weibo.com','Upgrade-Insecure-Requests':'1','Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
 remoteCookie = {'Cookie': 'SINAGLOBAL=180004545838.58127.1478226788825; TC-Ugrow-G0=02e35d9240e6933947925d24232af628; SSOLoginState=1484533995; TC-V5-G0=458f595f33516d1bf8aecf60d4acf0bf; wb_g_upvideo_1900964351=1; _s_tentry=login.sina.com.cn; Apache=4914359893033.586.1484533998141; ULV=1484533998155:8:2:1:4914359893033.586.1484533998141:1484014332879; TC-Page-G0=e2379342ceb6c9c8726a496a5565689e; UOR=www.csdn.net,widget.weibo.com,login.sina.com.cn; SCF=AunKxN5b-9Qj_8fHyis92yCtDUqeqE1j2tPOKNHhkkWvaaHsOYZP-QxFTjFndvhGUmQX-Xb7vsiPInnoKUwWkAA.; SUB=_2A251efdeDeRxGedH61IY9irPzj2IHXVWD2-WrDV8PUNbmtAKLU7QkW9OTxN_MGwdKyPi10rGDGtDF_OgzQ..; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWJOyNfRkxkNh0jS8bSgy.y5JpX5KMhUgL.Fo24eh54SoB0SK22dJLoI7y0UsH0IsLV9Btt; SUHB=010HR9Eb3Yh3k3; ALF=1516157581'}
 aricleSet = set()
@@ -104,7 +106,9 @@ def getTitle(content):
     return title
 
 def getArticleContent(id,cookie):
-    r = requests.get(articleDomain + id, cookies=cookie)
+    contentUrl = articleDomain + id
+    print contentUrl
+    r = requests.get(contentUrl, cookies=cookie)
     return r.text
 
 def outputFile(title,content):
@@ -118,11 +122,30 @@ def getArticleId(urlSet):
         id = url[url.index('%3D')+3:url.index('&')]
         idSet.add(id)
     return idSet
+#获取已经加载的文章id
+def getArticleDB():
+    areadyArticle = set()
+    try:
+        file = open(path+areadyFile,'r')
+        lines = file.readlines()
+        file.close()
+    except:
+        return set()
+    for line in lines:
+        areadyArticle.add(line.strip())
+    return areadyArticle
+
+def insertArticeDB(idsets):
+    file = open(path + areadyFile, 'wb')
+    for i in idsets:
+        if(i != ''):
+            file.write(i+'\n')
+    file.close()
 
 def process(articleList):
-    list = '<meta>'
+    list = '<link rel="shortcut icon" href="favorite.ico"><link rel="apple-touch-icon-precomposed" href="favorite.jpg">'
     for i in articleList:
-        content = getArticleContent(i, cookies)
+        content = getArticleContent(i, contentCookie)
         print content
         title = getTitle(content)
         list = list + '<a href="'+i+'.html">'+title+'</a><br/>'
@@ -131,9 +154,10 @@ def process(articleList):
 #cookies = getCookies(myWeiBo)
 
 print "Get Cookies Finish!( Num:%d)" % len(cookies)
-allArtile = set()
-for i in range(1,10):
+allArtileIds = getArticleDB()
+for i in range(1,2):
     urlSet = getArticleList(str(i), cookies)
-    allArtile = allArtile | getArticleId(urlSet)
-process(allArtile)
+    allArtileIds = allArtileIds | getArticleId(urlSet)
+insertArticeDB(allArtileIds)
+process(allArtileIds)
 #print allArtile   123
